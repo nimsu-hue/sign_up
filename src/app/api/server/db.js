3d4aeb525322128768e1f8083b269f61f675c192
Blitzkreig <crhlstl4nk09@gmail.com>
2024-10-24 14:09:17 -0600

    deploy with vercel

--- src/app/api/server/db.js
+++ src/app/api/server/db.js
@@ -1,11 +1,8 @@
-import { Pool } from 'pg';
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Sign-in',
-  password: '%M1p0$tSQLx',
-  port: 5432,
-});
+  connectionString: "postgres://default:7YLwlHDeX9dg@ep-plain-leaf-a4z2mpao-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+})
 
 export default pool;
