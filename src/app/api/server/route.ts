1674982379f492a211ff989bcc61e191b5172aff
Blitzkreig <crhlstl4nk09@gmail.com>
2024-10-25 15:40:21 -0600

    deploy with vercel

--- src/app/api/server/route.ts
+++ src/app/api/server/route.ts
@@ -2,17 +2,32 @@ import { NextResponse } from 'next/server';
 import pool from './db';
 
 export async function POST(req: Request) {
-  const { name, mail, password } = await req.json();
+    const { name, mail, password } = await req.json();
 
-  try {
-    const query = 'INSERT INTO users (name, mail, password) VALUES ($1, $2, $3) RETURNING *';
-    const values = [name, mail, password];
+    try {
+        const checkQuery = 'SELECT * FROM users WHERE mail = $1 OR name = $2';
+        const checkValues = [mail, name];
+        const checkResult = await pool.query(checkQuery, checkValues);
 
-    const result = await pool.query(query, values);
+        if (checkResult.rows.length > 0) {
+            return NextResponse.json({ 
+                message: 'El correo electrónico o el nombre de usuario ya están en uso.' 
+            }, { status: 409 });
+        }
 
-    return NextResponse.json({ message: 'Usuario creado exitosamente', user: result.rows[0] }, { status: 201 });
-  } catch (error) {
-    console.error('Error al crear el usuario:', error);
-    return NextResponse.json({ message: 'Error al crear el usuario', error: 'Error interno' }, { status: 500 });
-  }
+        const query = 'INSERT INTO users (name, mail, password) VALUES ($1, $2, $3) RETURNING *';
+        const values = [name, mail, password];
+        const result = await pool.query(query, values);
+
+        return NextResponse.json({ 
+            message: 'Usuario creado exitosamente', 
+            user: result.rows[0] 
+        }, { status: 201 });
+    } catch (error) {
+        console.error('Error al crear el usuario:', error);
+        return NextResponse.json({ 
+            message: 'Error al crear el usuario', 
+            error: 'Error interno' 
+        }, { status: 500 });
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,36 +34,47 @@ export default function Home() {
       <form
         onSubmit={handleSubmit}
         id="form-users"
-        className="bg-zinc-700 text-white w-full max-w-[600px] min-h-[300px] rounded-[20px] p-[15px] flex flex-col justify-center items-center gap-[20px]"
+        className="w-full max-w-[600px] min-h-[300px] rounded-[20px] p-[15px] flex flex-col justify-center items-center gap-[20px]"
       >
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Nombre Completo"
-          required
-          className="w-[300px] p-[10px] rounded-[5px] text-black"
-        />
-        <input
-          type="email"
-          id="mail"
-          value={mail}
-          onChange={(e) => setMail(e.target.value)}
-          placeholder="Correo Electronico"
-          required
-          className="w-[300px] p-[10px] rounded-[5px] text-black"
-        />
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Contraseña"
-          required
-          className="w-[300px] p-[10px] rounded-[5px] text-black"
-        />
-        <button type="submit" className="w-[300px] p-[10px] rounded-[5px] cursor-pointer bg-zinc-900 md:hover:bg-zinc-950">
+        <label className='w-[300px]'>
+          Nombre del usuario <span className='text-red-500'>*</span>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Smith Jhonson"
+            required
+            className="w-[300px] p-[10px] rounded-[5px] text-black mt-[10px] bg-zinc-100"
+          />
+        </label>
+        <label className='w-[300px]'>
+          Correo Electronico <span className='text-red-500'>*</span>
+          <input
+            type="email"
+            id="mail"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+            placeholder="username@gmail.com"
+            pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+"
+            required
+            className="w-[300px] p-[10px] rounded-[5px] text-black mt-[10px] bg-zinc-100"
+          />
+        </label>
+        <label className='w-[300px]'>
+          Contraseña <span className='text-red-500'>*</span>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="e8M(#F1J(3sQC-"
+            pattern="(?=.*\d).{8,}"
+            required
+            className="w-[300px] p-[10px] rounded-[5px] text-black mt-[10px] bg-zinc-100"
+          />
+        </label>
+        <button type="submit" className="w-[300px] p-[10px] rounded-[5px] cursor-pointer bg-zinc-900 md:hover:bg-zinc-950 text-white">
           Crear Cuenta
         </button>
       </form>
