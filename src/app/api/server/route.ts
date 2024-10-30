d9ce2587decfc2a0102e6596847c3fdbd90aadeb
Blitzkreig <crhlstl4nk09@gmail.com>
2024-10-30 07:58:04 -0600

    deploy with vercel

--- src/app/api/server/route.ts
+++ src/app/api/server/route.ts
@@ -1,22 +1,30 @@
 import { NextResponse } from 'next/server';
 import pool from './db';
+import bcrypt from 'bcrypt';
 
 export async function POST(req: Request) {
     const { name, mail, password } = await req.json();
+    const saltRounds: number = 10;
+
+    async function hashPassword(password: string): Promise<string> {
+        const hash: string = await bcrypt.hash(password, saltRounds);
+        return hash;
+    }
 
     try {
         const checkQuery = 'SELECT * FROM users WHERE mail = $1 OR name = $2';
         const checkValues = [mail, name];
         const checkResult = await pool.query(checkQuery, checkValues);
-
+        
         if (checkResult.rows.length > 0) {
             return NextResponse.json({ 
                 message: 'El correo electrónico o el nombre de usuario ya están en uso.' 
             }, { status: 409 });
         }
-
-        const query = 'INSERT INTO users (name, mail, password) VALUES ($1, $2, $3) RETURNING *';
-        const values = [name, mail, password];
+        
+        const hashedPassword = await hashPassword(password)
+        const query = 'INSERT INTO users (name, mail, password_hash) VALUES ($1, $2, $3) RETURNING *';
+        const values = [name, mail, hashedPassword];
         const result = await pool.query(query, values);
 
         return NextResponse.json({ 
